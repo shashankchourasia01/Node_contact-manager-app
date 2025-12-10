89e81ac31d6f3213ea6a58d6ae12799085b8f812
shashank chourasia01 <shashankchourasia0829@gmail.com>
2025-12-10 21:53:20 +0530

    qf

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const errorHandler = require('./middleware/errorHandler');
 dotenv.config();
 
 
@@ -9,6 +10,8 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactsRoute"));
+// Error handling middleware
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
