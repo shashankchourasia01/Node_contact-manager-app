e4916228f2de03e915ac1d419cff2814baa2753c
shashank chourasia01 <shashankchourasia0829@gmail.com>
2025-12-10 21:47:21 +0530

    new  fix

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,48 +1,50 @@
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access Public
-const getContacts = (req,res) => {
-    res.status(200).json({message: 'Get all contacts'});
+const getContacts = (req, res) => {
+  res.status(200).json({ message: "Get all contacts" });
 };
-
 
 //@desc create new contacts
 //@route POST /api/contacts
 //@access Public
-const createContacts = (req,res) => {
-    res.status(2001).json({message: 'Create new contacts'});
-};
+const createContacts = (req, res) => {
+  console.log("The request body is:", req.body);
 
+  //destructuring assignment
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+  res.status(201).json({ message: "Create new contacts" });
+};
 
 //get contacts by ID
 //@route POST /api/contacts/:id
 //@access Public
-const getContact = ((req,res) => {
-    res.status(200).json({message: ` Get contact for ${req.params.id}`});
-})
+const getContact = (req, res) => {
+  res.status(200).json({ message: ` Get contact for ${req.params.id}` });
+};
 
- 
-//update contacts 
+//update contacts
 //@route PUT /api/contacts/:id
 //@access Public
-const updateContact = ((req,res) => {
-    res.status(200).json({message: ` Update contact for ${req.params.id}`});
-})
+const updateContact = (req, res) => {
+  res.status(200).json({ message: ` Update contact for ${req.params.id}` });
+};
 
-
-//DELETE contacts 
+//DELETE contacts
 //@route DELETE /api/contacts/:id
 //@access Public
-const deleteContact = ((req,res) => {
-    res.status(200).json({message: ` Delete contact for ${req.params.id}`});
-})
-
-
+const deleteContact = (req, res) => {
+  res.status(200).json({ message: ` Delete contact for ${req.params.id}` });
+};
 
 module.exports = {
-    getContacts,
-    createContacts,
-    getContact,
-    updateContact,
-    deleteContact
+  getContacts,
+  createContacts,
+  getContact,
+  updateContact,
+  deleteContact,
 };
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use("/api/contacts", require("./routes/contactsRoute"));
 
 app.listen(port, () => {
