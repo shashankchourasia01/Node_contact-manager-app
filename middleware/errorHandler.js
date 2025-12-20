e943b267c364fb3abe5ee8d847594b849b07c15d
shashank chourasia01 <shashankchourasia0829@gmail.com>
2025-12-20 23:58:44 +0530

    errorHandler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -26,18 +26,20 @@ const errorHandler = (err, req, res, next) => {
       });
       case constants.UNAUTHORIZE:
       res.json({
-        title: "Not Found",
+        title: "Unauthorize",
         message: err.message,
         stackTrace: err.stack,
       });
       case constants.SERVER_ERROR:
       res.json({
-        title: "Not Found",
+        title: "Server Error",
         message: err.message,
         stackTrace: err.stack,
       });
 
     default:
+        console.log('No error all good');
+        
       break;
   }
 };
