db3ace3efd58d687313179b66697ccbeabcdb674
shashank chourasia01 <shashankchourasia0829@gmail.com>
2025-12-23 18:15:47 +0530

    user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler')
 const Users = require("../models/userModel")
+const bcrypt = require('bcrypt')
 
 //@desc Register a user
 //@route POST /api/user/register
@@ -10,7 +11,30 @@ const registerUser = asyncHandler(async (req,res) => {
         res.status(404)
         throw new Error('all fields are required')
     }
+    const userAvailable = await Users.findOne({email});
+    if( userAvailable){
+        res.status(400);
+        throw new Error("user already register")
+    }
+    //if user not available then create user with hashing his password using bcrypt library
+    const hashedPassword = await bcrypt.hash(password, 10)
+    console.log("hashed pass: ",hashedPassword);
+
+    //now create user
+    const user = await Users.create({
+        username,
+        email,
+        password: hashedPassword
+    })
+    console.log(`user created" ${user}`);
+    if(user){
+        res.status(201).json({ _id: user.id, email: user.email})
+    } else{
+        res.status(400);
+        throw new Error('user data was not valid')
+    }
     
+
     res.json({ message: "Register the user"})
 });
 
